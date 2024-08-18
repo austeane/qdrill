7b846bc4a442987d5f7ed3cd9b9f610c8ab75e50
Austin Wallace <austin.wallace@xgen.tech>
2024-08-18 13:42:37 -0700

    Adjust vercel.json and SvelteKit configuration for Node.js v18

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,12 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// Ensure the adapter for Vercel is configured correctly
-			runtime: 'nodejs18.x',
-			isr: {
-				expiration: false
-			}
-		})
-	}
+		adapter: adapter(),
+		prerender: {
+		  entries: []
+		}
+	  }
+	
 };
 
 export default config;

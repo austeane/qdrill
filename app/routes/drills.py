bc40d9cc3a5ea5b7cf7dc95e9d08307a3ba3404b
Austin Wallace <austeane@gmail.com>
2024-08-19 21:29:52 -0700

    Fix form submission issue
    Related to #8
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/austeane/qdrill/issues/8?shareId=XXXX-XXXX-XXXX-XXXX).

--- app/routes/drills.py
+++ app/routes/drills.py
@@ -8,26 +8,27 @@ drills_bp = Blueprint('drills', __name__)
 @drills_bp.route('/', methods=['POST'])
 def create_drill():
     data = request.get_json()
+    print("Request data: ", data)
     drill_schema = DrillSchema()
     errors = drill_schema.validate(data)
     if errors:
         return jsonify(errors), 400
     drill = drill_schema.load(data)
-    print("Drill: ", drill)
+    print("Created drill object: ", drill)
     db.session.add(drill)
     db.session.commit()
-    return drill_schema.jsonify(drill), 201
+    response = drill_schema.jsonify(drill)
+    print("Response data: ", response)
+    return response, 201
 
 @drills_bp.route('/', methods=['GET'])
 def get_drills():
     drills = Drill.query.all()
     drill_schema = DrillSchema(many=True)
-    print("Drills: ", drills)
     return drill_schema.jsonify(drills), 200
 
 @drills_bp.route('/<int:id>', methods=['GET'])
 def get_drill(id):
     drill = Drill.query.get_or_404(id)
     drill_schema = DrillSchema()
-    print("Drill: ", drill)
     return drill_schema.jsonify(drill), 200

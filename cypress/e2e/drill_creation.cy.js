ddb2ba179051caafe2967d4a52abde4743aeb32e
Austin Wallace <austeane@gmail.com>
2024-08-19 23:42:17 -0700

    Fix front end fields to use snake_case
    Related to #8
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/austeane/qdrill/issues/8?shareId=XXXX-XXXX-XXXX-XXXX).

--- cypress/e2e/drill_creation.cy.js
+++ cypress/e2e/drill_creation.cy.js
@@ -3,15 +3,15 @@ describe('Drill Creation', () => {
     cy.visit('/drills/create');
 
     cy.get('input#name').type('Test Drill');
-    cy.get('input#briefDescription').type('A brief description of the test drill');
-    cy.get('textarea#detailedDescription').type('A detailed description of the test drill');
-    cy.get('input#skillLevel').type('Beginner');
+    cy.get('input#brief_description').type('A brief description of the test drill');
+    cy.get('textarea#detailed_description').type('A detailed description of the test drill');
+    cy.get('input#skill_level').type('Beginner');
     cy.get('input#complexity').type('Low');
-    cy.get('input#suggestedLength').type('10 minutes');
-    cy.get('input#numberOfPeople').type('5');
-    cy.get('input#skillsFocusedOn').type('Passing, Shooting');
-    cy.get('input#positionsFocusedOn').type('Forward, Midfield');
-    cy.get('input#videoLink').type('http://example.com/video');
+    cy.get('input#suggested_length').type('10 minutes');
+    cy.get('input#number_of_people').type('5');
+    cy.get('input#skills_focused_on').type('Passing, Shooting');
+    cy.get('input#positions_focused_on').type('Forward, Midfield');
+    cy.get('input#video_link').type('http://example.com/video');
 
     cy.get('button[type="submit"]').click();
 

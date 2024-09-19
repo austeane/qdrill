ca8257ebdfda226e3073a0082d9735fdde62533d
Austin Wallace <austeane@gmail.com>
2024-09-18 17:08:52 -0700

    updating for import

--- src/routes/drills/DrillForm.svelte
+++ src/routes/drills/DrillForm.svelte
@@ -441,7 +441,7 @@
             on:save={(event) => handleDiagramSave(event, index)} 
             id={`diagram-canvas-${index}`} 
             data={diagram} 
-            {index}
+            index={diagramIndex}
             showSaveButton={true} 
           />
           <div class="mt-2 flex justify-between">

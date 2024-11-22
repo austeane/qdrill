3972af93e21d68e973b417d80db78c92b062898e
Austin Wallace <austeane@gmail.com>
2024-11-22 11:12:06 -0800

    buy me a coffee

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -35,6 +35,16 @@
         <a href="/" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Back to Home
         </a>
+        
+        <div class="mt-4">
+          <a href="https://www.buymeacoffee.com/austeane" target="_blank">
+            <img 
+              src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" 
+              alt="Buy Me A Coffee" 
+              style="height: 60px !important;width: 217px !important;"
+            >
+          </a>
+        </div>
       </div>
     </div>
   </section>

1201c5a542086e41d511ef998238f16d1deab4a7
Austin Wallace <austeane@gmail.com>
2024-11-27 22:33:56 -0800

    using analytics

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,12 @@
 import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ locals }) {
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
+
 	return {
 		analyticsId: dev ? undefined : 'VERCEL_ANALYTICS_ID',
 		session: await locals.getSession()

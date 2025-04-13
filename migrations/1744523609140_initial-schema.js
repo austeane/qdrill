5e08533108d7d5b44556a919da66a8f2d6d6a61b
Austin Wallace <austeane@gmail.com>
2025-04-12 23:20:08 -0700

    updating lots

--- migrations/1744523609140_initial-schema.js
+++ migrations/1744523609140_initial-schema.js
@@ -7,12 +7,22 @@ export const shorthands = undefined;
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
+ * This migration applies the initial database schema.
+ * Add schema creation statements here if starting from scratch.
  */
-export const up = (pgm) => {};
+export const up = (pgm) => {
+	// Example: pgm.createTable('users', { id: 'id', name: { type: 'varchar(100)', notNull: true } });
+	// Currently empty as schema might exist or be managed elsewhere initially.
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
+ * This migration reverts the initial database schema.
+ * Add statements to drop tables/types created in the 'up' function.
  */
-export const down = (pgm) => {};
+export const down = (pgm) => {
+	// Example: pgm.dropTable('users');
+	// Currently empty.
+};

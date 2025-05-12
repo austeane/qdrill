1af35ac78185db66120b82a969a778a519ad02cd
Austin <austeane@gmail.com>
2025-05-11 23:50:58 -0600

    adding sentry

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'qdrill',
+				project: 'javascript-sveltekit'
+			}
+		}),
+		sveltekit()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	optimizeDeps: {
-		include: ['@sveltejs/kit', '@sveltejs/adapter-vercel', '@excalidraw/excalidraw', 'react', 'react-dom'],
+		include: [
+			'@sveltejs/kit',
+			'@sveltejs/adapter-vercel',
+			'@excalidraw/excalidraw',
+			'react',
+			'react-dom'
+		],
 		exclude: ['@mapbox/node-pre-gyp']
 	},
 	resolve: {

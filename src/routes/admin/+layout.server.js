64d4bf05aa466deffe3c435017bf6ed58eb27fc3
Austin Wallace <austeane@gmail.com>
2025-07-18 19:24:43 +0000

    Remove admin migration page

--- src/routes/admin/+layout.server.js
+++
@@ -1,9 +0,0 @@
-import { dev } from '$app/environment';
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-	// Only allow access in development mode
-	if (!dev) {
-		throw redirect(307, '/');
-	}
-}
